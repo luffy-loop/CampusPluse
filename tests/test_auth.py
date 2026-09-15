8642a60c5141f8abdeafc4a044d99e6f6a2330e0
luffy-loop <poojasrikandhula@gmail.com>
2026-09-15 17:22:36 +0530

    Configure test secret key

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,6 +5,7 @@ from app import app
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
+    app.config["SECRET_KEY"] = "test-secret-key"
 
     with app.test_client() as client:
         yield client
